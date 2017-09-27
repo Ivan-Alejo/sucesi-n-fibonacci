0ea8dd292f9ae5bfc1320cdb811352edfc9db019
Ivan-Alejo <31460009+Ivan-Alejo@users.noreply.github.com>
2017-09-27 17:08:19 -0500

    coloque la forma de resolver la sucesion Fibinacci de manera mas larga

--- src/SucesiónFibonacci.java
+++ src/SucesiónFibonacci.java
@@ -1,26 +1,73 @@
 public class SucesiónFibonacci {
 
-public static void main(String[] args){
-SucesiónFibonacci x = new SucesiónFibonacci();
-System.out.println("la sucesion fibonacci de 12 por recursividad es: "+x.fibonacci(12));
-System.out.println("la sucesion fibonacci de 12 por ciclo es: "+x.fibonacciCiclo(12));
-}
- public int fibonacci(int n){
-    if (n==1 || n==2){
-        return 1;
-    }else{
-        return  fibonacci(n-1)+fibonacci(n-2);
+    public static void main(String[] args) {
+        SucesiónFibonacci x = new SucesiónFibonacci();
+        System.out.println("la sucesion fibonacci de 12 por recursividad es: " + x.fibonacci(12));
+        System.out.println("la sucesion fibonacci de 12 por ciclo es: " + x.fibonacciCiclo(12));
+        System.out.println("la sucesion fibonacci de 12 por forma normal es: " + x.fibonormal(12));
     }
- }
- public int fibonacciCiclo(int n){
-     int fibo=0, primero=1,segundo=0;
-     while(n>0){
-         fibo= primero+segundo;
-         primero=segundo;
-         segundo=fibo;
-         n--;
 
-     }
-     return fibo;
- }
+    public int fibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public int fibonacciCiclo(int n) {
+        int fibo = 0, primero = 1, segundo = 0;
+        while (n > 0) {
+            fibo = primero + segundo;
+            primero = segundo;
+            segundo = fibo;
+            n--;
+
+        }
+        return fibo;
+    }
+
+
+    public int fibonormal(int n) {
+        int fibo = 0, primero = 1, segundo = 0;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+        primero = segundo;
+        segundo = fibo;
+        fibo = primero + segundo;
+
+        System.out.println(fibo);
+
+        return fibo;
+    }
+
 }
